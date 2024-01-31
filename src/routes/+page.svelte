568ec9a48aedb49c980d031a59ee8a5ad0d35a37
Developer-Zahid <coderexpertzahid@gmail.com>
2024-01-31 18:30:51 +0600

    Adde chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,100 @@
 <script>
+	import { onMount } from 'svelte';
 	import BagCheckedIcon from '$lib/assets/icons/chart-icon/BagCheckedIcon.svelte'
 	import TradeUpIcon from '$lib/assets/icons/chart-icon/TradeUpIcon.svelte'
 	import WalletCashIcon from '$lib/assets/icons/chart-icon/WalletCashIcon.svelte'
 	import WalletIcon from '$lib/assets/icons/chart-icon/WalletIcon.svelte'
+
+	let totalSalesChartElement;
+	var totalSalesChartOptions = {
+		series: [
+			{
+				name: "Session Duration",
+				data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+			},
+			{
+				name: "Page Views",
+				data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
+			},
+			{
+				name: 'Total Visits',
+				data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
+			}
+		],
+		chart: {
+			height: 350,
+			type: 'line',
+			zoom: {
+				enabled: false
+			},
+		},
+		dataLabels: {
+			enabled: false
+		},
+		stroke: {
+			width: [5, 7, 5],
+			curve: 'straight',
+			dashArray: [0, 8, 5]
+		},
+		title: {
+			text: 'Page Statistics',
+			align: 'left'
+		},
+		legend: {
+			tooltipHoverFormatter: function(val, opts) {
+				return val + ' - <strong>' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + '</strong>'
+			}
+		},
+		markers: {
+			size: 0,
+			hover: {
+				sizeOffset: 6
+			}
+		},
+		xaxis: {
+			categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+				'10 Jan', '11 Jan', '12 Jan'
+			],
+		},
+		tooltip: {
+			y: [
+			{
+				title: {
+				formatter: function (val) {
+					return val + " (mins)"
+				}
+				}
+			},
+			{
+				title: {
+				formatter: function (val) {
+					return val + " per session"
+				}
+				}
+			},
+			{
+				title: {
+				formatter: function (val) {
+					return val;
+				}
+				}
+			}
+			]
+		},
+		grid: {
+			borderColor: '#f1f1f1',
+		}
+	};
+
+	onMount(() => {
+		import('apexcharts').then(module => {
+			const ApexCharts = module.default;
+
+			var totalSalesChart = new ApexCharts(totalSalesChartElement, totalSalesChartOptions)
+			totalSalesChart.render()
+		});
+	});
+
 </script>
 
 <svelte:head>
@@ -13,7 +105,11 @@
 <section class="pt-4 pb-5">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-lg-7"></div>
+			<div class="col-lg-7">
+				<div class="chart-card">
+					<div bind:this={totalSalesChartElement}></div>
+				</div>
+			</div>
 			<div class="col-lg-5">
 				<div class="row">
 					<div class="col-sm-6 col-lg-12 col-xxl-6 mb-4">
